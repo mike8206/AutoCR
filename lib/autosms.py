6b493bc28f387bbb0a711105e22c9b8fbb1b50d7
MikeWu <78641606+mike8206@users.noreply.github.com>
2022-05-13 00:59:08 +0800

    Update autosms.py

--- lib/autosms.py
+++ lib/autosms.py
@@ -10,11 +10,11 @@ TIMEOUT = 5
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--start-maximized')
 
-def main(url_dict, cr_id_path, phone_path, google_secret_path, google_token_path, chrome_driver_path):
+def main(url_dict, cr_id_path, phone_path, google_secret_path, google_token_path, google_cal_id, chrome_driver_path):
     # read portal credential from txt file
     cridpw = {}
     try:
-        with open(cr_id_path, encoding="ANSI") as f:
+        with open(cr_id_path, encoding="UTF-8") as f:
             idpwpin = f.read().splitlines()
             # id pw (帳號 密碼)
             cridpw['id']=idpwpin[0]
@@ -23,17 +23,20 @@ def main(url_dict, cr_id_path, phone_path, google_secret_path, google_token_path
         raise ValueError('CR帳號密碼檔案錯誤!!')
 
     try:
-        with open(phone_path, encoding="ANSI") as f:
+        with open(phone_path, encoding="UTF-8") as f:
             phone_list = f.read()
     except:
         raise ValueError('簡訊檔案錯誤!!')
 
-    # get event from google calendar
-    SMSstring = google_calendar(google_secret_path, google_token_path)
-    # chrome driver
-    driver = webdriver.Chrome(chrome_driver_path, options = chrome_options)
-    driver.implicitly_wait(TIMEOUT)
-    # login using chrome and get the session id
-    session_id = login(driver, url_dict, cridpw)
-    # send sms
-    send_sms(driver, url_dict, session_id, phone_list, SMSstring)
+    if google_cal_id[0]:
+        # get event from google calendar
+        SMSstring = google_calendar(google_secret_path, google_token_path, google_cal_id)
+        # chrome driver
+        driver = webdriver.Chrome(chrome_driver_path, options = chrome_options)
+        driver.implicitly_wait(TIMEOUT)
+        # login using chrome and get the session id
+        session_id = login(driver, url_dict, cridpw)
+        # send sms
+        send_sms(driver, url_dict, session_id, phone_list, SMSstring)
+    else:
+        raise ValueError('未設定Google日曆ID!!')
