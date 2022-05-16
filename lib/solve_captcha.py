31cb68fb9d26621d944d09c2b7253fa334321fa8
MikeWu <78641606+mike8206@users.noreply.github.com>
2022-05-16 10:14:50 +0800

    Update solve_captcha.py

--- lib/solve_captcha.py
+++ lib/solve_captcha.py
@@ -1,21 +1,17 @@
 from requests import get
 import ddddocr
 
-image_path = 'temp.gif'
-
 headers={
    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
 }
 
 def downloadFile(url):
     response = get(url, headers=headers)
-    with open(image_path, 'wb') as f:
-        f.write(response.content)
+    img_bytes = response.content
+    return img_bytes
 
 def solve_captcha(captcha_img_ele):
-    downloadFile(captcha_img_ele.get_attribute('src'))
+    img_bytes = downloadFile(captcha_img_ele.get_attribute('src'))
     ocr = ddddocr.DdddOcr()
-    with open(image_path, 'rb') as f:
-        img_bytes = f.read()
     captcha = ocr.classification(img_bytes)
     return captcha
