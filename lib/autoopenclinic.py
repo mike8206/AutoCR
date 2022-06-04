531607d1f96023bf5bc6627d4a2e2a3c4ed619f7
MikeWu <78641606+mike8206@users.noreply.github.com>
2022-06-04 09:12:00 +0800

    Update autoopenclinic.py

--- lib/autoopenclinic.py
+++ lib/autoopenclinic.py
@@ -6,16 +6,15 @@ from lib.clinic.clinic_open import clinic_open
 
 def main(url_dict, vs_id_path, chrome_driver_path, ie_driver_path):
     # read portal credential from txt file
-    vsidpw = {}
     vsidpw = readIdPwPin(vs_id_path)
     # chrome driver
     driver = web_driver_setting('chrome', chrome_driver_path, 'max')
     # login using chrome and get the session id
     session_id = login(driver, url_dict, vsidpw)
     driver.close()
+    # IE driver
+    driver_ie = web_driver_setting('ie', ie_driver_path, '')
     try:
-        # IE driver
-        driver_ie = web_driver_setting('ie', ie_driver_path, '')
         # open IE for open clinic
         clinic_open(driver_ie, url_dict, session_id)
         driver_ie.close()
